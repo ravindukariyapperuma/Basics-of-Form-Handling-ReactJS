606ff00ab7d840267f63d162e99aaa37d1a23eec
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-25 00:57:58 +0530

    Username handler added

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,13 +9,23 @@ class Form extends Component {
              username: ''
         }
     }
+
+    handleUsernameChange = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+    }
     
     render() {
         return (
             <form>
                 <div>
                     <label>Username</label>
-                    <input type='text' />
+                    <input
+                     type="text" 
+                     value={this.state.username} 
+                     onChange={this.handleUsernameChange} 
+                     />
                 </div>
             </form>
         )
